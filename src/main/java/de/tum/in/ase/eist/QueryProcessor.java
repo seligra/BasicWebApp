b6142d3b516cc73de3a96334154152484b64ebc1
selinagraf <selina.graf@tum.de>
2022-07-14 13:22:38 +0200

    irrelevant change

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -11,10 +11,12 @@ public class QueryProcessor {
             return "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
-        } else if (query.contains("name")) {
-           return "MyTeam";
+        } else if (query.toLowerCase().contains("name")) {
+           return "Eisteist";
         } else { // TODO extend the programm here
             return "";
         }
     }
+    int x = 206 + 208;
+
 }
